03fde733cfa57985bdcfcd5100d6f4e9204b7753
bezerkt1 <bezerkt1@hotmail.com>
2023-10-27 09:10:18 +0200

    Implement fortune telling

--- src/components/form/Summary.jsx
+++ src/components/form/Summary.jsx
@@ -3,44 +3,33 @@ const Summary = ({
   setCounter,
   setShowQuestions,
   setShowSummary,
+  updateForm,
+  fortuneData,
 }) => {
   // Function to go back to beginning of survey
   const goToStart = () => {
     setShowQuestions(true);
     setShowSummary(false);
     setCounter(0);
+    updateForm("fortuneNumber", 0);
   };
 
-  const generateFortune = () => {
-    switch (formData.fortuneNumber) {
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      default:
-        return `You entered an invalid input! You shall be cursed!`;
-    }
+  const fortuneToString = () => {
+    const summary = fortuneData[parseInt(formData.fortuneNumber) - 1].summary;
+    let fortune = "";
+    summary.forEach((aString) => {
+      fortune += aString;
+      if (formData[summary.indexOf(aString)] !== undefined) {
+        fortune += formData[summary.indexOf(aString)];
+      }
+    });
+    return fortune;
   };
 
   return (
     <>
       <div className="summary">
-        {
-          // using entries function to make an array with keys and value that can then use the map function
-          Object.entries(formData).map(([key, value]) => {
-            return (
-              <p>
-                {key}: {value}
-              </p>
-            );
-          })
-        }
+        <p>{fortuneToString()}</p>
         <button onClick={goToStart}>Back to Start</button>
       </div>
     </>
@@ -48,5 +37,3 @@ const Summary = ({
 };
 
 export default Summary;
-
-// need to fix goToStart func so it goes back to first question
